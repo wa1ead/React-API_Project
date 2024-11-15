d7314784ca5935e000db34fe43b6a42a7a267e6f
wa1ead <waliddraa2@gmail.com>
2024-11-15 21:06:23 +0100

    displaying user website

--- src/UserList.js
+++ src/UserList.js
@@ -19,16 +19,34 @@ export default function UserList() {
     <div className="flex flex-row gap-20 w-[60%] m-auto h-[100vh] justify-center items-center flex-wrap py-10">
       {listOfUsers.map((user) => (
         <div
-          className="flex flex-col border items-center border-gray-500 rounded-md p-2 w-60 h-60"
+          className="flex flex-col border items-center border-gray-500 bg-blue-200 rounded-md p-4 w-60 h-60"
           key={user.id}
         >
           <span className="inline-flex items-center justify-center size-[62px] font-semibold rounded-full leading-none border border-gray-200 bg-white text-gray-800 shadow-sm dark:bg-neutral-900 dark:border-neutral-700 dark:text-white">
             {user.name.split(" ")[0][0] + user.name.split(" ")[1][0]}
           </span>
-          <p className="text-center font-bold text-lg my-2">{user.name}</p>
-          <p className='text-nowrap text-sm'>
-            Email: <span className='font-semibold'>{user.email}</span>
+          <p className="text-center font-bold text-lg my-2 text-nowrap">
+            {user.name}
+            <br />
+            <span className="font-normal">
+              <em>({user.username})</em>
+            </span>
           </p>
+
+          <p className="text-nowrap text-sm my-2">
+            Email: <span className="font-semibold">{user.email}</span>
+          </p>
+          <div className="flex items-center justify-center gap-2 text-blue-700">
+            <i class="fa-solid fa-globe"></i>
+            <a
+              href={`http://www.${user.website}`}
+              target="_blank"
+              rel="noreferrer"
+              className=" underline"
+            >
+              {user.website}
+            </a>
+          </div>
         </div>
       ))}
     </div>
