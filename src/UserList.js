d43ef87292630d7b991cb9cd621b8b254539a2cc
wa1ead <waliddraa2@gmail.com>
2024-11-15 19:29:26 +0100

    displaying data on the user list file

--- src/UserList.js
+++ src/UserList.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function UserList() {
-  return (
-    <div>UserList</div>
-  )
+  //THE LIST OF USERS STATE
+  const [listOfUsers, setListOfUsers] = useState([]);
+
+  //FETCHING DATA WHEN THE PAGE RENDERED FOR THE 1ST TIME
+  useEffect(() => {
+    //FETCHING USERS DATA FROM JSONPLACEHOLDER USING AXIOS
+    axios("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((res) => setListOfUsers(res.data))
+      .catch((err) => err.message);
+  }, []);
+
+  return <div>{listOfUsers}</div>;
 }
